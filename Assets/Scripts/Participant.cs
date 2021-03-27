7c25a4f441e84f63007481eaee23eb04341f99e8
Dino <dbecaj@fordham.edu>
2021-03-27 15:15:14 -0400

    Playing around with classes -- separation of concerns

---
+++ Assets/Scripts/Participant.cs
@@ -0,0 +1,54 @@
+﻿using VRClassroom;
+
+namespace VRClassroom
+{
+    public class Participant
+    {
+        private string firstName;
+        private string lastName;
+        private string email;
+        private string status;
+        private string password;
+
+        public Participant(string firstName, string lastName, string email, string password, string status)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.password = password;
+            this.status = status;
+        }
+
+        public bool validateNewAccount()
+        {
+            DatabaseManager credentials = new DatabaseManager();
+
+            return credentials.CheckNewCredentials(this);
+        }
+
+        public string GetFirstName()
+        {
+            return firstName;
+        }
+
+        public string GetLastName()
+        {
+            return lastName;
+        }
+
+        public string GetEmail()
+        {
+            return email;
+        }
+
+        public string GetStatus()
+        {
+            return status;
+        }
+
+        public string GetPassword()
+        {
+            return password;
+        }
+    }
+}
